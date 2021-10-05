eb0a0768f2bf6bc1bd9062bc0f56e5742c095237
Ayyar Khalilov <super.ayar-04@yandex.ru>
2021-10-05 20:46:37 +0300

    Solution for "Sequences"

---
+++ sequences.cpp
@@ -0,0 +1,35 @@
+
+// Индивидуальные задачи на тему "Последовательности"
+// #15
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter number sequence length: ";
+    int n;
+    std::cin >> n;
+
+    if (n > 0)
+        std::cout << "Enter sequence of " << n << " elements: ";
+
+    int min_odd = 0;
+    for ( int i = 0; i < n; ++i )
+    {
+        int k;
+        std::cin >> k;
+        if ( k % 2 == 1 )
+            if ( !min_odd || k < min_odd )
+                min_odd = k;
+    }
+
+    std::cout << '\n';
+    if ( !min_odd )
+        std::cout << "The sequence doesn't contain any odd number.\n";
+    else
+        std::cout << "Minimum odd number in the sequence is " << min_odd << '\n';
+    std::cout << '\n';
+
+    system("pause");
+    return 0;
+}
