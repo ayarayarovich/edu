9af1ccfc0d21c439148e1f18fd70e8f74b8bb001
Ayyar Khalilov <super.ayar-04@yandex.ru>
2021-10-20 20:56:06 +0300

    Solution for "Arrays"

---
+++ arrays.cpp
@@ -0,0 +1,34 @@
+
+// Индивидуальные задачи на тему "Массивы"
+// #15
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter array length: ";
+    int n;
+    std::cin >> n;
+
+    std::cout << "Enter sequence of " << n << " elements:\n";
+    int arr[1000];
+    for (int i = 0; i < n; ++i)
+        std::cin >> arr[i];
+
+    bool brr[1000];
+    for (int i = 0; i < n; ++i) {
+        int ds = 0;
+        for (int d = 1; d <= arr[i] / 2; ++d)
+            ds += d * (arr[i] % d == 0);
+
+        bool isPerfect = arr[i] == ds;
+        brr[i] = !isPerfect;
+    }
+
+    for (int i = 0; i < n; ++i)
+        std::cout << brr[i] << ' ';
+    std::cout << '\n';
+
+    system("pause");
+    return 0;
+}
